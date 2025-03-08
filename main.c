b4cb6026112a6cc244456d6911a8501598630be8
B1aho <wheelsurgut00@gmail.com>
2025-03-08 21:21:18 +0300

    Add project structure

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    // Load config files, if any.
+
+    // Run command loop.
+    // lsh_loop();
+    // Clear stdin
+
+    // Perform any shutdown/cleanup.
+    return EXIT_SUCCESS;
+}
